44834fdeac6fdef6b0ba9fd3c44a07f363285900
701tkdgns <55741859+701tkdgns@users.noreply.github.com>
2024-02-20 22:55:07 +0900

    update client

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+interface photoCard {
+    onPic: string;
+    onTitle: string;
+    onLink: string;
+    onDays: string;
+    onContents: string;
+}
+
+const Card: React.FC<photoCard> = ({ onPic, onTitle, onLink, onDays, onContents }) => {
+    const [overlayActive, setOverlayActive] = useState(false);
+    const onImgClick = () => {
+        window.open(onLink, '_blank')
+    };
+    const toggleOverlay = () => {
+        setOverlayActive(!overlayActive);
+    }
+    return (
+        <div className='psh__portfolio-card'>
+            <div className='psh__portfolio-card-header'>
+                <h3>{onTitle}</h3>
+            </div>
+            <div className='psh__portfolio-card-body'>
+                <div>
+                    <div className='psh__portfolio-card-photo' onClick={onImgClick} onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay}>
+                        <img src={onPic} alt={onTitle} />
+                        <div className={`psh__portfolio-card-photo-overlay ${overlayActive ? 'active' : ''}`}>바로가기</div>
+                    </div>
+                    <div className='psh__portfolio-card-text'>
+                        <span className='psh__portfolio-card-text-contents'><h5>프로젝트명 :</h5>{onTitle}</span>
+                        <span className='psh__portfolio-card-text-contents'><h5>개발일자 :</h5>{onDays}</span>
+                        <span className='psh__portfolio-card-text-contents'><h5>역할 :</h5>{onContents}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,8 @@ declare module '*.jpg' {
     const data : any;
     export default data;
 }
+
+declare module '*.png' {
+    const data : any;
+    export default data;
+}
